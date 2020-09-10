7c3ff6bacd94606e3aac9c41669115594e56de12
Miguel Munoz <memg92@gmail.com>
2020-09-10 20:30:37 -0300

    figuring out full columns

--- column.js
+++ column.js
@@ -3,14 +3,23 @@ export class Column {
     this.columnSquares = [null, null, null, null, null, null];
   }
 
+  isFull() {
+    return this.columnSquares[0] !== null;
+  }
+
   add(playerNum) {
-    for (let i = 5; i >= 0; i--) {
-      if (this.columnSquares[i] === null) {
-        this.columnSquares[i] = playerNum;
-        break;
+    if (this.isFull()) {
+      return;
+    } else {
+      for (let i = 5; i >= 0; i--) {
+        if (this.columnSquares[i] === null) {
+          this.columnSquares[i] = playerNum;
+          break;
+        }
       }
     }
   }
+
   getTokenAt(rowIndex) {
     return this.columnSquares[rowIndex];
   }
