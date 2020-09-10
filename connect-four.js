68e32ec9e465cdce1facabcdbdd77b1b10a3e33b
Lane Smit <lanesmit88@outlook.com>
2020-09-10 14:29:49 -0500

    wrote game class

--- connect-four.js
+++ connect-four.js
@@ -1,27 +1,52 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+import { Game } from './game.js';
   let game = undefined;
+  let boardHolder = document.getElementById('board-holder');
+  let gameName = document.getElementById('game-name');
+  function updateUI() {
+    if(game === undefined) {
+      boardHolder.classList.add('is-invisible')
+    } else {
+      boardHolder.classList.remove('is-invisible');
+      gameName.innerHTML = getName(game) 
+    }
+  };
+  
+  window.addEventListener("DOMContentLoaded", (event) => {
+
   const p1 = document.getElementById("player-1-name");
   const p2 = document.getElementById("player-2-name");
   const newGame = document.getElementById("new-game");
   newGame.disabled = true;
 
+function trueNewGame() {
+  newGame.disabled = true;
+}
+
+function falseNewGame() {
+  newGame.disbled = false;
+}
+
   p1.addEventListener("keyup", (event) => {
     if (p1 && p2) {
-      newGame.disabled = false;
+      falseNewGame();
     } else {
-      newGame.disabled = true;
+      trueNewGame();
     }
   });
 
   p2.addEventListener("keyup", (event) => {
     if (p1 && p2) {
-      newGame.disabled = false;
+      falseNewGame();
     } else {
-      newGame.disabled = true;
+      trueNewGame();
     }
   });
 
   newGame.addEventListener("click", event => {
-    game =
+    game = new Game('','');
+    trueNewGame();
+    updateUI()
   })
+  
 });
+
---
+++ game.js
@@ -0,0 +1,6 @@
+export class Game {
+  constructor(p1name, p2name) {
+    this.p1name = p1name;
+    this.p2name = p2name;
+  }
+}
